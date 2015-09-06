de8108c6a4bc223516acd5c8fb479c99d31b4190
Adam Collins <adam@m104.us>
2015-09-05 22:09:15 -0700

    core bin/ support added

--- core.d/05_bin.sh
+++ core.d/05_bin.sh
@@ -1,10 +1,18 @@
+# shell-config bin PATH support
+corebinpath="$SHELL_CONFIG_BASEDIR/bin"
+if test -d "$corebinpath"; then
+  export SHELL_CONFIG_MODS="$SHELL_CONFIG_MODS core-bin"
+  export PATH="$corebinpath:$PATH"
+fi
+unset corebinpath
+
 # User ~/bin PATH support
 #
 # must have ~/bin directory to enable
 #
 binpath="$HOME/bin"
 if test -d "$binpath"; then
-  export SHELL_CONFIG_MODS="$SHELL_CONFIG_MODS bin"
+  export SHELL_CONFIG_MODS="$SHELL_CONFIG_MODS home-bin"
   export PATH="$binpath:$PATH"
 fi
 unset binpath
